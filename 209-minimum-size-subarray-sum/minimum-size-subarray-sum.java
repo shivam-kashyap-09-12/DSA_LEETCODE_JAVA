644a3873bed26fe0fc660a3e7c735d0ebbe87afe
Shivam Kashyap <shivam.0912.kashyap@gmail.com>
2026-09-21 00:39:51 +0530

    Time: 1 ms (99.80%) | Memory: 69.2 MB (66.88%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,9 +1,9 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
 
-        int left = 0;
         int sum = 0;
-        int min = Integer.MAX_VALUE;
+        int left = 0;
+        int minLength = Integer.MAX_VALUE;
 
         for (int right = 0; right < nums.length; right++) {
 
@@ -11,13 +11,13 @@ class Solution {
 
             while (sum >= target) {
 
-                min = Math.min(min, right - left + 1);
+                minLength = Math.min(minLength, right - left + 1);
 
                 sum -= nums[left];
                 left++;
             }
         }
 
-        return min == Integer.MAX_VALUE ? 0 : min;
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
 }
